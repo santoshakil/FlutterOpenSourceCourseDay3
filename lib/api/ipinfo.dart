f4c557ef849ccf388fd7c1b9ae380e923b01df9f
Santo Shakil <shakil.mezbah@gmail.com>
2021-04-30 00:41:10 +0600

    ..

--- lib/api/ipinfo.dart
+++ lib/api/ipinfo.dart
@@ -24,3 +24,34 @@ Future<IpDataModel> getData() async {
     return null;
   }
 }
+
+var jsonDataFromAPI = '''
+{
+    "ip": "192.168.0.1",
+    "version": "IPv4",
+    "city": "Dhaka",
+    "region": "Dhaka Division",
+    "region_code": "C",
+    "country": "BD",
+    "country_name": "Bangladesh",
+    "country_code": "BD",
+    "country_code_iso3": "BGD",
+    "country_capital": "Dhaka",
+    "country_tld": ".bd",
+    "continent_code": "AS",
+    "in_eu": false,
+    "postal": "1000",
+    "latitude": 23.7272,
+    "longitude": 90.4093,
+    "timezone": "Asia/Dhaka",
+    "utc_offset": "+0600",
+    "country_calling_code": "+880",
+    "currency": "BDT",
+    "currency_name": "Taka",
+    "languages": "bn-BD,en",
+    "country_area": 144000.0,
+    "country_population": 161356039.0,
+    "asn": "AS55492",
+    "org": "Dhaka Fiber Net Limited"
+}
+''';
